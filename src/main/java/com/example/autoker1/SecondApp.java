00bf8a569760c5da772cb963f1dd9c2607568e0c
lengyelbence01 <bence.lengyel01@gmail.com>
2024-01-08 20:51:10 +0100

    Másodlagos nézet legenerálása.

--- src/main/java/com/example/autoker1/SecondApp.java
+++ src/main/java/com/example/autoker1/SecondApp.java
@@ -1,4 +1,15 @@
 package com.example.autoker1;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class SecondApp {
+public class SecondApp extends Application {
+    public Stage stage;
+    public void start(Stage stage) throws Exception{
+        this.stage=stage;
+        FXMLLoader fxmlLoader=new FXMLLoader(SecondApp.class.getResource("autohozzaadas-view.fxml"));
+        Scene scene=new Scene(fxmlLoader.load());
+
+    }
 }
