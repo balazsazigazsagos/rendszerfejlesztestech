8be448ca4fe2fd73c990031988066c937a88560c
balazsazigazsagos <balazs.kvetko@gmail.com>
2024-01-08 19:16:29 +0100

    fx:id-ikhez változók létrehozása a MainController-ben action-ökhöz eljárások létrehozása

--- src/main/java/com/example/autoker1/MainController.java
+++ src/main/java/com/example/autoker1/MainController.java
@@ -1,14 +1,29 @@
 package com.example.autoker1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 public class MainController {
+    public AnchorPane anchorPane;
+    public ImageView imgShowedOnScreen;
+    public Button addCartoShopBTN;
+    public Label osszesar;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void disAppearImage(MouseEvent mouseEvent) {
+    }
+
+    public void addNewCartoShop(ActionEvent event) {
     }
 }
