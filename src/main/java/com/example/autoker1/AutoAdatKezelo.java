21217223431a018bca9c97ad05ce0f5ceb008b10
balazsazigazsagos <balazs.kvetko@gmail.com>
2024-01-08 19:40:42 +0100

    Fájl beolvasása, beolvasáshoz szükséges változók megadása

--- src/main/java/com/example/autoker1/AutoAdatKezelo.java
+++ src/main/java/com/example/autoker1/AutoAdatKezelo.java
@@ -1,4 +1,21 @@
 package com.example.autoker1;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AutoAdatKezelo {
+    public ArrayList<Auto> osszesAuto=new ArrayList<>();
+    public ArrayList<String[]> sorokAdatai=new ArrayList<>();
+    public String filename;
+    int nextIndex=1;
+
+    public AutoAdatKezelo(){
+        filename="src/main/resources/autok.csv";
+        File file=new File(filename);
+
+
+
+    }
+
 }
