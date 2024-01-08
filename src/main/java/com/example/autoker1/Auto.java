e50ea0cc051fc0653f6c22a844e2b0cd5708a533
balazsazigazsagos <balazs.kvetko@gmail.com>
2024-01-08 19:34:03 +0100

    Auto példányhoz változók megadása.

--- src/main/java/com/example/autoker1/Auto.java
+++ src/main/java/com/example/autoker1/Auto.java
@@ -1,4 +1,10 @@
 package com.example.autoker1;
 
 public class Auto {
+    public Integer auto_id;
+    public String marka;
+    public String modell;
+    public Integer evjarat;
+    public Integer ar;
+    public String kep_link;
 }
