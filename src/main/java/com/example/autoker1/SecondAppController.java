76e344d3efcc2395b5b8a75b80b2528bc9407ec2
lengyelbence01 <bence.lengyel01@gmail.com>
2024-01-08 21:24:06 +0100

    Változók létrehozása a második nézethez.

--- src/main/java/com/example/autoker1/SecondAppController.java
+++ src/main/java/com/example/autoker1/SecondAppController.java
@@ -1,5 +1,18 @@
 package com.example.autoker1;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class SecondAppController extends MainController {
     public SecondApp secondApp;
+    public ImageView img;
+    public Button submitBTN;
+    public Label labelKepLink;
+    public Button tallozas;
+    public TextField textfield1;
+    public TextField textfield2;
+    public TextField textfield3;
+    public TextField textfield4;
 }
