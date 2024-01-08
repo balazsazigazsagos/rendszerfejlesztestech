713e281e243a1452830c9d89a9ced46e1e4c11b5
balazsazigazsagos <balazs.kvetko@gmail.com>
2024-01-08 19:35:43 +0100

    Auto példányhoz konstruktor létrehozás.

--- src/main/java/com/example/autoker1/Auto.java
+++ src/main/java/com/example/autoker1/Auto.java
@@ -7,4 +7,15 @@ public class Auto {
     public Integer evjarat;
     public Integer ar;
     public String kep_link;
+
+    //konstruktor létrehozása
+    public Auto(Integer auto_id, String marka, String modell, Integer evjarat, Integer ar,
+                String kep_link){
+        this.auto_id=auto_id;
+        this.marka=marka;
+        this.modell=modell;
+        this.evjarat=evjarat;
+        this.ar=ar;
+        this.kep_link=kep_link;
+    }
 }
