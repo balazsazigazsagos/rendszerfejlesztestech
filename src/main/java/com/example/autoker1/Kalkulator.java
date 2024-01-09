327f039049a91cd323b1a5fa940ba1774045c8e2
T1M4rP <timarpeti1234@gmail.com>
2024-01-09 12:09:05 +0100

    Eventek létrehozása

--- src/main/java/com/example/autoker1/Kalkulator.java
+++ src/main/java/com/example/autoker1/Kalkulator.java
@@ -4,3 +4,44 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+
+public class Controller {
+
+    @FXML
+    private TextField textField;
+
+    @FXML
+    private Text savedNumbers;
+
+    private String firstNumber = "";
+
+    private String currentNumber = "";
+
+    private String calculationType;
+
+    @FXML
+    void addAction(ActionEvent event) {
+        calculationSetup("+");
+    }
+
+    @FXML
+    void minusAction(ActionEvent event) {
+        calculationSetup("-");
+    }
+
+    @FXML
+    void divideAction(ActionEvent event) {
+        calculationSetup("/");
+    }
+
+    @FXML
+    void multiplicationAction(ActionEvent event) {
+        calculationSetup("*");
+    }
+
+    public void calculationSetup(String calculationType){
+        this.calculationType = calculationType;
+        firstNumber = currentNumber;
+        currentNumber = "";
+        savedNumbers.setText(firstNumber + " " + calculationType);
+    }
